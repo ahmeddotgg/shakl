cc68b13ca4da54c4f69968f7ee7a6282efca3f52
Ahmed Said <ahmeddsaidd@proton.me>
2025-10-23 13:13:41 +0300

    refactor: simpelify download flow

--- src/modules/checkout/item.tsx
+++ src/modules/checkout/item.tsx
@@ -1,5 +1,4 @@
-import { FileDown, Loader } from "lucide-react";
-import { useState } from "react";
+import { FileDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import type { TransactionProduct } from "~/supabase/index";
 
@@ -8,34 +7,6 @@ interface Props {
 }
 
 export const Item = ({ product }: Props) => {
-  const [disabled, setDisabled] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const handleDownload = async () => {
-    if (disabled) return;
-
-    try {
-      setLoading(true);
-      const res = await fetch("/api/increment-download", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ productId: product.id }),
-      });
-
-      const data = await res.json();
-      if (!data.success) {
-        console.error("Failed to increment download count:", data.error);
-        return;
-      }
-      setDisabled(true);
-      window.open(product.file_url, "_blank");
-    } catch (err) {
-      console.error("Request failed:", err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
     <div className="relative flex gap-2 rounded-2xl bg-secondary/60 p-2">
       <img
@@ -44,17 +15,13 @@ export const Item = ({ product }: Props) => {
         className="size-28 rounded-xl object-cover"
       />
 
-      <div>
-        <h2 className="line-clamp-2 font-semibold text-xs md:text-lg min-[325px]:text-sm">
+      <div className="flex flex-col">
+        <h2 className="line-clamp-2 flex-1 font-semibold text-xs md:text-lg min-[325px]:text-sm">
           {product.title}
         </h2>
 
-        <Button onClick={handleDownload} disabled={loading || disabled}>
-          {loading ? (
-            <Loader className="size-5 animate-spin" />
-          ) : (
-            <FileDown className="size-5" />
-          )}
+        <Button onClick={() => window.open(product.file_url, "_blank")}>
+          <FileDown className="size-5" />
           <span className="hidden min-[290px]:inline">Download</span>
         </Button>
       </div>
