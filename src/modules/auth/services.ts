cc2da5aa7b8db246561101ceac8b61ad3a393835
Ahmed Said <ahmeddsaidd@proton.me>
2025-10-23 13:39:56 +0300

    fix: update email redirect url

--- src/modules/auth/services.ts
+++ src/modules/auth/services.ts
@@ -5,7 +5,6 @@ export async function signUp(email: string, password: string, name: string) {
     email,
     password,
     options: {
-      emailRedirectTo: process.env.SERVER_URL,
       data: {
         name: name,
       },
